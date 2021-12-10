91b0f3eab27ebc7a636424126852f5faa16f8dab
ArminAwista <75390942+ImArmin@users.noreply.github.com>
2021-12-10 20:09:34 +0000

    I have completed my house assignment.

--- Sketch.java
+++ Sketch.java
@@ -1,35 +1,133 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  // Called once at the beginning of execution. Size Is Set
   public void settings() {
-	// put your size call here
+
+	// Size Of House Picture
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Called once at the beginning of execution. Background Of House Is Set
   public void setup() {
-    background(210, 255, 173);
+    background(51, 153, 255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  // Drawing The House
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Sun
+    fill(255, 255, 0);
+    ellipse(50, 50, 80, 80);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Hill
+    fill(0, 153, 0);
+    ellipse(330, 350, 200, 200);
+
+    // Hill
+    fill(0, 153, 0);
+    ellipse(100, 370, 200, 200);
+
+    // Hill
+    fill(0, 153, 0);
+    ellipse(30, 380, 200, 200);
+
+    // Hill
+    fill(0, 153, 0);
+    ellipse(370, 380, 200, 200);
+
+    // Grass
+    fill(102, 255, 102);
+    rect(0, 340, 400, 400);
+
+    // Cloud
+    fill(255, 255, 255);
+    ellipse(240, 30, 140, 40);
+
+    // Cloud
+    fill(255, 255, 255);
+    ellipse(290, 40, 140, 40);
+
+    // Cloud
+    fill(255, 255, 255);
+    ellipse(190, 37, 140, 40);
+
+    // House Bricks
+    fill(255, 204, 51);
+    rect(100, 155, 200, 200);
+
+    // Door
+    fill(153, 102, 0);
+    rect(175, 280, 50, 75);
+
+    // Window
+    fill(255, 255, 255);
+    rect(112, 280, 50, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(107, 280, 60, 5);
+
+    // Window
+    fill(255, 255, 255);
+    rect(238, 280, 50, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(233, 280, 60, 5);
+
+    // Window
+    fill(255, 255, 255);
+    rect(112, 200, 75, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(107, 200, 85, 5);
+
+    // Window
+    fill(255, 255, 255);
+    rect(213, 200, 75, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(208, 200, 85, 5);
+
+    // Door Handle
+    fill(255, 204, 0);
+    ellipse(215, 315, 10, 10);
+
+    // House Chimny
+    fill(255, 204, 51);
+    rect(250, 80, 25, 70);
+
+    // Chimny Exit
+    fill(51, 0, 0);
+    rect(245, 80, 35, 5);
+
+    // House Roof
+    fill(51, 0, 0);
+    triangle(200, 75, 325, 175, 75, 175);
+
+    // Tree Leaves
+    fill(0, 102, 0);
+    ellipse(65, 220, 100, 100);
+
+    // Tree Trunk
+    fill(102, 51, 0);
+    rect(55, 270, 20, 100);
+
+    // Bush
+    fill(0, 102, 0);
+    ellipse(315, 330, 60, 60);
+
+    // Bush
+    fill(0, 102, 0);
+    ellipse(340, 350, 60, 60);
+
+    // Bush
+    fill(0, 102, 0);
+    ellipse(300, 350, 60, 60);
   }
   
   // define other methods down here.
